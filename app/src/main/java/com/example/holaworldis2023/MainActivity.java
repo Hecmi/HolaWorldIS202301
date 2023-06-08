820ea517a6cd8831cc302b95ab706e9818e8f9fe
Hecmi <120283562+Hecmi@users.noreply.github.com>
2023-06-08 16:16:02 -0500

    Mensaje en toast

--- app/src/main/java/com/example/holaworldis2023/MainActivity.java
+++ app/src/main/java/com/example/holaworldis2023/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.holaworldis2023;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void btEnviar(View view) {
+        EditText txtNombre = (EditText)findViewById(R.id.txtNombre);
+        Toast.makeText(this.getApplicationContext(), "Hola " + txtNombre.getText().toString(), Toast.LENGTH_LONG).show();
     }
 }
