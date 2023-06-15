b9e0585367d72fb4dbfff3a822590ef278ece09f
Hecmi <120283562+Hecmi@users.noreply.github.com>
2023-06-15 17:48:12 -0500

    Consumo de web services usando volley y paquete realizado por el Ing.

--- app/src/main/java/com/example/holaworldis2023/LoginProductsActivity.java
+++ app/src/main/java/com/example/holaworldis2023/LoginProductsActivity.java
@@ -36,11 +36,14 @@ public class LoginProductsActivity extends AppCompatActivity implements Asynchta
     EditText txtPassword;
     String access_token;
 
+    Integer error_type;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_products);
 
+        error_type = 0;
         access_token = "";
 
         txtUsername = (EditText)findViewById(R.id.txtUsername);
@@ -54,7 +57,7 @@ public class LoginProductsActivity extends AppCompatActivity implements Asynchta
 
         String url = "https://api.uealecpeterson.net/public/login";
 
-        Map<String, String> datos = new HashMap<String, String>();
+        /*Map<String, String> datos = new HashMap<String, String>();
         datos.put("correo", username);
         datos.put("clave", password);
         datos.put("fuente", "1");
@@ -63,7 +66,66 @@ public class LoginProductsActivity extends AppCompatActivity implements Asynchta
                 url,
                 datos, LoginProductsActivity.this, LoginProductsActivity.this);
 
-        ws.execute("POST");
+        ws.execute("POST");*/
+
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+        StringRequest getRequest = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>()
+                {
+                    @Override
+                    public void onResponse(String response) {
+
+                        JSONObject jObject = null;
+                        try {
+                            jObject = new JSONObject(response);
+                        } catch (JSONException e) {
+                            throw new RuntimeException(e);
+                        }
+
+                        if(!jObject.has("error")){
+                            try {
+                                access_token = jObject.getString("access_token");
+                            } catch (JSONException e) {
+                                throw new RuntimeException(e);
+                            }
+
+                            Intent intent = new Intent(LoginProductsActivity.this, ProductsActivity.class);
+
+                            //Enviar datos a la siguiente actividad
+                            Bundle b = new Bundle();
+                            b.putString("EMAIL", txtUsername.getText().toString());
+                            b.putString("PASSWORD", txtPassword.getText().toString());
+                            b.putString("FUENTE", "1");
+                            b.putString("TOKEN", access_token);
+
+                            intent.putExtras(b);
+
+                            startActivity(intent);
+                        }
+                        //Transformar/castear JSON
+                    }
+                },
+                new Response.ErrorListener()
+                {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        error_type = error.networkResponse.statusCode;
+                        Log.i("E", "Error");
+                    }
+                }
+        ) {
+            @Override
+            public Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String>  params = new HashMap<String, String>();
+                params.put("correo", username);
+                params.put("clave", password);
+                params.put("fuente", "1");
+
+                return params;
+            }
+        };
+        queue.add(getRequest);
     }
 
     @Override
