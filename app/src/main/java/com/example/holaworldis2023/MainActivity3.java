c038ad5eaa29860e740e2057b99fcde236a7e1e5
Hecmi <120283562+Hecmi@users.noreply.github.com>
2023-06-15 13:33:36 -0500

    Enviar datos de variables a otra actividad mediante el bundle y el intent

--- app/src/main/java/com/example/holaworldis2023/MainActivity3.java
+++ app/src/main/java/com/example/holaworldis2023/MainActivity3.java
@@ -3,12 +3,28 @@ package com.example.holaworldis2023;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity3 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main2);
+
+        TextView txtMensaje = (TextView)findViewById(R.id.txtMensaje);
+
+        Bundle bundle = this.getIntent().getExtras();
+
+        txtMensaje.setText("Hola!, Bienvenido \n "
+                + "Nombre: " + bundle.getString("NOMBRE") + "\n"
+                + "Password: " + bundle.getString("PASSWORD") + "\n"
+                + "Genero: " + bundle.getString("GENERO") + "\n"
+                + "Notificaciones: " + bundle.getString("NOTIFICACIONES")
+        );
+
     }
 }
